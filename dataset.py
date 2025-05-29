7ea92ebbffb69697200f8683e10319ecf5d4f24c
Vera <vera.podestatova@seznam.cz>
2025-05-29 11:19:23 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,8 +1,6 @@
 import os
 import torch
 import torchaudio
-import numpy as np
-import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from torch import nn
 
@@ -10,7 +8,6 @@ class AudioDataset(Dataset):
     def __init__(self,
                  directory=None,
                  sr=44100,
-                 # sr=22050,
                  seg_len = 262144,
                  stereo=False,
                  train=True,
@@ -25,34 +22,17 @@ class AudioDataset(Dataset):
         self.ds_percent = ds_percent
 
         files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".wav")]
-        # dataset index which divides files for training and testing calculated by percentage of the dataset files
-        ds_idx = int(len(files) * (ds_percent / 100))
+        ds_idx = int(len(files) * (ds_percent / 100)) # Dataset index which divides files for training and testing
 
         if self.train:
-            # self.files = files[:ds_idx]  # First part of the dataset for training
-            self.files = files[:1]  # First part of the dataset for training
+            self.files = files[:ds_idx]  # First part of the dataset for training
         else:
-            # self.files = files[ds_idx:]  # Remaining files for testing
-            self.files = files[1:]  # Remaining files for testing
+            self.files = files[ds_idx:]  # Remaining files for testing
 
     def load_segment(self, audio_file):
         audio, sr = torchaudio.load(audio_file, normalize=True)
-        # min max normalization to ensure the dynamic range is within the values of [-1, 1]:
+        # Min max normalization to ensure the dynamic range is within the values of [-1, 1]:
         audio = (audio - audio.min()) / (audio.max() - audio.min()) * 2 - 1
-        # maximum normalization:
-        # audio = audio / audio.abs().max()
-        # range normalization:
-        # audio = (audio - audio.min()) / (audio.max() - audio.min()) * (1 - 0) + 1
-        # unit norm normalization:
-        # audio = audio / audio.norm(p=2)
-        # DRC:
-        # audio = torchaudio.transforms.Vol(0.5)(audio)
-        # mean center:
-        # audio = audio - audio.mean()
-        # log compression:
-        # audio = torch.log(torch.abs(audio) + 1e-6)
-        # clip normalization:
-        # audio = audio.clamp(-1, 1)
         if sr != self.sr:
             raise ValueError(f"Expected sample rate of {self.sr} but got {sr}")
         if self.stereo:
@@ -61,8 +41,6 @@ class AudioDataset(Dataset):
             audio = nn.functional.pad(audio, (0, self.seg_len - audio.size(1)))
         elif audio.size(1) > self.seg_len:
             idx = torch.randint(0, audio.size(1) - self.seg_len, (1,)).item()
-            # proc zrovna 1548? proc nejdem od zacatku segmentu??
-            # idx = 1548
             audio = audio[:, idx:idx + self.seg_len]
         return audio
 
@@ -80,11 +58,8 @@ class AudioDataset(Dataset):
     def __getitem__(self, index):
         return self.load_segment(self.files[index])
 
-
 if __name__ == "__main__":
-    # dataset = AudioDataset(directory="GTZAN_dataset", sr=22050, seg_len=262144)
-    dataset = AudioDataset(directory="./GTZAN_dataset/", sr=22050, seg_len=262144)
-    dataset.print_params()  # self created function to print the parameters of the dataset
-    # usage of dataloader
+    dataset = AudioDataset(directory="./IDMT-SMT-GUITAR/", sr=44100, seg_len=262144)
+    dataset.print_params()
     dataloader = DataLoader(dataset, batch_size=4)
     print(next(iter(dataloader)).shape)
