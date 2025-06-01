d7ea017cee6e68150448e247a07de29617b59cb5
247800 <114662833+247800@users.noreply.github.com>
2025-06-01 20:50:09 +0200

    Update sampling_utils.py

--- utils/sampling_utils.py
+++ utils/sampling_utils.py
@@ -10,18 +10,6 @@ def get_time_schedule(sigma_min=1e-5, sigma_max=12, T=100, rho=10):
     sigma_i[T] = 0
     return sigma_i
 
-def get_noise(t, S_tmin=1e-5, S_tmax=12, S_churn=None, idx=0):
-    if S_churn is None:
-        S_churn = t[idx:idx+1]
-    if S_churn < S_tmin:
-        gamma_i = 0
-    elif S_churn > S_tmax:
-        gamma_i = 0
-    else:
-        N = torch.randn(1)
-        gamma_i = min(S_churn / N, np.sqrt(1) - 1)
-    return gamma_i
-
 def load_checkpoint(path, device, model):
     state_dict = torch.load(path, map_location=device, weights_only=False)
     try:
